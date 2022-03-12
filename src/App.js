aecca1eb9b03cff51e917ab33c6fa6f5262b4f3e
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-11 22:20:15 -0800

    Main container that wraps all children

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from './components/Products';
+
+
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React Router</h2>
+      <Router>
+        <nav>
+          <Link to="/">Home </Link>
+          <Link to="/about">About </Link>
+          <Link to="/products">Products </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products/*" element={<Products />} />
+          {/* <Route path="*" element={<h2>Page not found</h2>} /> */}
+          <Route path="*" element={<Navigate to="/about" />} />
+        </Routes>
+
+      </Router>
     </div>
   );
 }
